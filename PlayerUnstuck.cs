ff5c939845b4f95d975f2131916012e499487a5f
Arifloof <dustswier@gmail.com>
2020-08-13 01:27:32 -0600

    v2.05 - Updated for Exiled 2.0.12

--- PlayerUnstuck.cs
+++ PlayerUnstuck.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Exiled.API.Enums;
 using Exiled.API.Features;
 using UnityEngine;
 
@@ -24,7 +25,7 @@ namespace ArithFeather.PlayerUnstuck {
 		}
 
 		public override string Author => "Arith";
-		public override Version Version => new Version("2.04");
+		public override Version Version => new Version("2.05");
 
 		public Dictionary<string, StuckInRoom>
 			ScpTryingToEscape = new Dictionary<string, StuckInRoom>(Config.CacheSize);
@@ -39,20 +40,17 @@ namespace ArithFeather.PlayerUnstuck {
 
 			foreach (var doorPoint in _rawPoints) {
 				var point = doorPoint.Value;
-				var roomName = point.RoomType;
+				var roomType = point.RoomType;
 
 				for (int i = 0; i < roomCount; i++) {
 					var room = rooms[i];
 
-					if (room.Name.StartsWith(roomName, StringComparison.InvariantCultureIgnoreCase))
+					if (room.RoomType == roomType)
 						_fixedPoints.Add(doorPoint.Key,
-							new DoorPoint(string.Empty, room.Transform.TransformPoint(point.Position)));
+							new DoorPoint(roomType, room.Transform.TransformPoint(point.Position)));
 
 				}
 			}
-
-			var surfaceTransform = GameObject.Find("Root_*&*Outside Cams").transform;
-			_fixedPoints.Add("nuke_surface", new DoorPoint(string.Empty, surfaceTransform.TransformPoint(_rawPoints["nuke_surface"].Position)));
 		}
 
 		private void Player_InteractingDoor(Exiled.Events.EventArgs.InteractingDoorEventArgs ev) {
@@ -77,10 +75,10 @@ namespace ArithFeather.PlayerUnstuck {
 		}
 
 		private class DoorPoint {
-			public readonly string RoomType;
+			public readonly RoomType RoomType;
 			public readonly Vector3 Position;
 
-			public DoorPoint(string roomType, Vector3 position) {
+			public DoorPoint(RoomType roomType, Vector3 position) {
 				this.RoomType = roomType;
 				Position = position;
 			}
@@ -88,28 +86,25 @@ namespace ArithFeather.PlayerUnstuck {
 
 		private readonly Dictionary<string, DoorPoint> _fixedPoints = new Dictionary<string, DoorPoint>();
 
-		// Root_*&*Outside Cams = Surface
 		private readonly Dictionary<string, DoorPoint> _rawPoints = new Dictionary<string, DoorPoint>
 		{
-			{"lcz_armory", new DoorPoint("LCZ_Armory", new Vector3(2.468124f, 1.43f, -0.01200104f))},
-			{"012", new DoorPoint("lcz_012", new Vector3(5.32489f, 1.430002f, -6.420892f))},
-			{"914", new DoorPoint("lcz_914", new Vector3(1.833687f, 1.430001f, 0.1032865f))},
+			{"lcz_armory", new DoorPoint(RoomType.LczArmory, new Vector3(2.468124f, 1.43f, -0.01200104f))},
+			{"012", new DoorPoint(RoomType.Lcz012, new Vector3(5.32489f, 1.430002f, -6.420892f))},
+			{"914", new DoorPoint(RoomType.Lcz914, new Vector3(1.833687f, 1.430001f, 0.1032865f))},
+			{"hid", new DoorPoint(RoomType.HczHid, new Vector3(0.06840611f, 1.429993f, -9.714242f))},
+			{"079_first", new DoorPoint(RoomType.Hcz079, new Vector3(15.22645f, -3.113281f, 0.02222576f))},
+			{"079_second", new DoorPoint(RoomType.Hcz079, new Vector3(6.521089f, -3.144531f, -14.89046f))},
+			{"106_bottom", new DoorPoint(RoomType.Hcz106, new Vector3(21.49849f, -18.66949f, -20.07209f))},
+			{"106_primary", new DoorPoint(RoomType.Hcz106, new Vector3(28.74622f, 1.329468f, 0.02032089f))},
+			{"106_secondary", new DoorPoint(RoomType.Hcz106, new Vector3(29.03088f, 1.329468f, -29.51972f))},
+			{"049_armory", new DoorPoint(RoomType.Hcz049, new Vector3(6.269577f, 265.4324f, 6.756578f))},
+			{"nuke_armory", new DoorPoint(RoomType.HczNuke, new Vector3(-3.545931f, 401.43f, 18.09708f))},
+			{"hcz_armory", new DoorPoint(RoomType.HczArmory, new Vector3(2.363129f, 1.429993f, 0.1472318f))},
+			{"096", new DoorPoint(RoomType.Hcz096, new Vector3(-1.850082f, 1.429993f, -0.1229479f))},
 
-			{"hid", new DoorPoint("HCZ_Hid", new Vector3(0.06840611f, 1.429993f, -9.714242f))},
-			{"hcz_457", new DoorPoint("HCZ_457", new Vector3(-1.850082f, 1.429993f, -0.1229479f))},
-			{"079_first", new DoorPoint("hcz_079", new Vector3(15.22645f, -3.113281f, 0.02222576f))},
-			{"079_second", new DoorPoint("hcz_079", new Vector3(6.521089f, -3.144531f, -14.89046f))},
-			{"106_bottom", new DoorPoint("hcz_106", new Vector3(21.49849f, -18.66949f, -20.07209f))},
-			{"106_primary", new DoorPoint("hcz_106", new Vector3(28.74622f, 1.329468f, 0.02032089f))},
-			{"106_secondary", new DoorPoint("hcz_106", new Vector3(29.03088f, 1.329468f, -29.51972f))},
-			{"049_armory", new DoorPoint("HCZ_049", new Vector3(6.269577f, 265.4324f, 6.756578f))},
-			{"nuke_armory", new DoorPoint("HCZ_Nuke", new Vector3(-3.545931f, 401.43f, 18.09708f))},
-			{"hcz_armory", new DoorPoint("HCZ_Room3ar", new Vector3(2.363129f, 1.429993f, 0.1472318f))},
-			{"096", new DoorPoint("HCZ_457", new Vector3(-1.850082f, 1.429993f, -0.1229479f))},
+			{"intercom", new DoorPoint(RoomType.EzIntercom, new Vector3(7.382059f, -0.4788208f, 1.433885f))},
 
-			{"intercom", new DoorPoint("ez_intercom", new Vector3(7.382059f, -0.4788208f, 1.433885f))},
-
-			{"nuke_surface", new DoorPoint("Surface", new Vector3(40.67078f, -11.0451f, -36.2037f))}
+			{"nuke_surface", new DoorPoint(RoomType.Surface, new Vector3(40.67078f, -11.0451f, -36.2037f))}
 		};
 	}
 }
